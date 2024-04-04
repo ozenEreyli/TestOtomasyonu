82bd2477d8b7357177c20e543386bc13d41dfe7d
ozenEreyli <153092233+ozenEreyli@users.noreply.github.com>
2024-04-04 15:09:23 +0300

    Update Main.java

--- hw21/src/test/java/tests/Main.java
+++ hw21/src/test/java/tests/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args){
 
-        org.testng.TestNG.main(new String[]{"-d", "target/test-classes","tests.Test"});
+        org.testng.TestNG.main(new String[]{"-d", "target/test-classes","tests.XpathTest"});
     }
 }
